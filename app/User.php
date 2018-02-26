3073ce8a7f5a6b48b6be0deff4ed6dc34a7ac0ee
klim0v <klim0v-sergey@yandex.ru>
2018-02-27 00:30:19 +0300

    initial orchid

---
+++ app/User.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App;
+
+use Orchid\Platform\Core\Models\User as BaseUser;
+
+/**
+ * App\User
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property string $password
+ * @property string|null $remember_token
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @property string|null $last_login
+ * @property array $permissions
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Orchid\Platform\Core\Models\Role[] $roles
+ * @method static \Illuminate\Database\Eloquent\Builder|\Orchid\Platform\Core\Models\User filtersApply($filters = array())
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereLastLogin($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User wherePermissions($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
+class User extends BaseUser
+{
+    
+}
